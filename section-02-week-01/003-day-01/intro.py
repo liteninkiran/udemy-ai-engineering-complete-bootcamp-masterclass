c1b9b1dab3b7752b9afceb1c08b960718b59c817
Kiran Anand <liteninkiran@hotmail.com>
2025-08-08 19:09:48 +0100

    003 - Day 1: Introduction to Python and Development Setup

---
+++ section-02-week-01/003-day-01/intro.py
@@ -0,0 +1,22 @@
+# Integer
+age = 25
+
+# Float
+height = 5.9
+
+# String
+name = "Dave"
+greeting = "Hello " + name
+
+# List (square brackets)
+numbers = [1, 2, 3, 4]
+names = ["Alice", "Jack"]
+
+# Tuple (round brackets)
+coordinates = (10, 20)
+
+# Dictionary
+person = { "name": "John", "age": 25}
+
+# Boolean
+is_student = False
