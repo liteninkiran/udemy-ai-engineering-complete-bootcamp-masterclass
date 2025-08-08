4286b619a7d467dd7d46b468c69658122cda1079
Kiran Anand <liteninkiran@hotmail.com>
2025-08-08 23:24:34 +0100

    004 - Day 2: Control Flow in Python

---
+++ section-02-week-01/004-day-02/day2_1.py
@@ -0,0 +1,43 @@
+# Example 1: Checking a condition
+# num = -50
+# if num > 0:
+#     print("Positive Number")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print("Negative Number")
+
+# Example 2: Nested conditions
+# age = 25
+# if age > 18:
+#     if age < 30:
+#         print("Young Adult")
+#     else:
+#         print("Adult")
+
+# Example 3: Loop Over Items
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# Example 4: Loop Over Range
+# for i in range(10): #[0,1,2,3,4]
+#     print(i)
+
+# Example 5: While Loop
+# count = 5
+# while count > 0:
+#     print(count)
+#     count -= 1
+
+# Example 6: Continue
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# Example 7: Break
+# for i in range(10):
+#     if i == 7:
+#         break
+#     print(i)
